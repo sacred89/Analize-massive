6fe30a24cee0d58147f1d3b07ca6b1550ea6c40c
sacred89 <sacred_89@bk.ru>
2016-06-13 21:25:14 +0400

    All Ok

---
+++ src/model/Row.java
@@ -0,0 +1,14 @@
+package model;
+
+/**
+ * Created by Егор on 13.06.2016.
+ */
+public class Row {
+    Object[] row ;
+
+    public Row(String[] massive,LogicScheme scheme) {
+
+        row = new Object[length];
+
+    }
+}
