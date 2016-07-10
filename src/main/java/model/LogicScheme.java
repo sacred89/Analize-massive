e1c8ab24416c608160dcf5b5a5b460c42628e718
egor <sacred_89@bk.ru>
2016-07-10 09:07:44 +0300

    Add  maven

---
+++ src/main/java/model/LogicScheme.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LogicScheme {
+
+    private List<String> columnNames;
+    private List<TypeValues> dataType;
+
+    public LogicScheme() {
+        columnNames = new ArrayList<>();
+        dataType = new ArrayList<>();
+    }
+
+    public void appendColumn(String name, TypeValues typeValues) {
+        columnNames.add(name);
+        dataType.add(typeValues);
+    }
+
+    public TypeValues getTypeValues(int index) {
+        return dataType.get(index);
+    }
+
+    public String getColumnName(int index) {
+        return columnNames.get(index);
+    }
+
+}
---
+++ src/main/java/model/TypeValues.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by Егор on 13.06.2016.
+ */
+public enum  TypeValues {
+    STR,INT,DOUBLE,DATE;
+}
